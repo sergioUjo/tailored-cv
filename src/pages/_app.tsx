604c90e6d8ef89fc76c534aa25c7e4660f4e2c6e
Sérgio Araújo <sergiozaraujoz98@gmail.com>
2023-06-06 17:12:41 +0000

    Added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,20 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
-import { PT_Serif } from 'next/font/google';
+import { PT_Serif } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 const font = PT_Serif({
-  weight: '400',
-  subsets: ['latin'],
+  weight: "400",
+  subsets: ["latin"],
 });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-      <main className={font.className}>
-        <Component {...pageProps} />
-      </main>
-  )
+    <main className={font.className}>
+      <Component {...pageProps} />
+      <Analytics />
+    </main>
+  );
 };
 
 export default api.withTRPC(MyApp);
