60ace1cb347c7aa4602a4b7d753b7e54b578d315
Sérgio Araújo <sergiozaraujoz98@gmail.com>
2023-06-21 10:30:01 +0000

    Corrigir quadrados de overview

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -8,11 +8,11 @@ import { FaCoins } from "@react-icons/all-files/fa/FaCoins";
 
 function Index() {
   const style =
-    "flex h-44 w-44 items-center flex-col justify-center rounded-lg border border-gray-100 bg-white text-center text-base text-black shadow hover:text-primary-600 hover:shadow-lg";
+    "flex h-44 items-center flex-col justify-center rounded-lg border border-gray-100 bg-white text-center text-base text-black shadow hover:text-primary-600 hover:shadow-lg";
   return (
     <AppLayout>
       <h1 className={"mb-4 text-4xl font-bold text-primary-600"}>Overview</h1>
-      <div className={"flex flex-wrap gap-4"}>
+      <div className={"grid grid-cols-2 gap-4 sm:grid-cols-4  lg:grid-cols-6"}>
         <Link className={style} href={"/app"}>
           <RiDashboardLine size={40} />
           Overview
